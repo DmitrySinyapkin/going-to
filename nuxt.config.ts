f9fc0bf8fba42784eda1a5f86b83709438b3f274
DmitrySinyapkin <dsinyapkin@mail.ru>
2023-07-20 14:55:30 +0300

    added vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,22 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     ['@pinia/nuxt', { autoImports: ['defineStore'] }],
+    '@invictus.codes/nuxt-vuetify',
   ],
   imports: {
     dirs: ['./stores']
+  },
+  vuetify: {
+    vuetifyOptions: {},
+    moduleOptions: {
+      /* nuxt-vuetify module options */
+      treeshaking: true,
+      useIconCDN: true,
+
+      /* vite-plugin-vuetify options */
+      styles: true,
+      autoImport: true,
+      useVuetifyLabs: true, 
+    }
   }
 })
