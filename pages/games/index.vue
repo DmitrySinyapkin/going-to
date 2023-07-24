5d2f85d181d54042daf12a30b4b6df91a8be6a18
DmitrySinyapkin <dsinyapkin@mail.ru>
2023-07-24 19:02:16 +0300

    add lazy loading to games catalog

--- pages/games/index.vue
+++ pages/games/index.vue
@@ -1,19 +1,27 @@
 <template>
     <section class="tw-p-2">
-        <h1>Games</h1>
+        <h1 class="tw-mb-10">Games</h1>
         <section class="tw-w-full tw-flex tw-flex-wrap tw-gap-4">
             <GameCard
                 v-for="game in gamesStore.gamesList"
                 :key="game.slug"
                 :game="game"
-            ></GameCard>
+            />
+            <ObserverComponent @intersect="intersected" />
         </section>
     </section>
 </template>
 
 <script setup lang='ts'>
     import GameCard from '../../components/games/GameCard.vue';
+    import ObserverComponent from '../../components/common/ObserverComponent.vue';
 
     const gamesStore = useGamesStore()
-    await gamesStore.getGamesList() 
+    await gamesStore.getGamesList()
+    
+    const intersected = async() => {
+        if (gamesStore.nextPageUrl) {
+            await gamesStore.getNextPage()
+        }
+    }
 </script>
