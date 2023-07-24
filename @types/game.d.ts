dac4f043e76ad87c47b9ee47efc0e0a106c4719d
DmitrySinyapkin <dsinyapkin@mail.ru>
2023-07-24 16:33:03 +0300

    added games types and improved store

--- @types/game.d.ts
+++ @types/game.d.ts
@@ -1,3 +1,56 @@
-interface Game {
-
+interface GamesResponse {
+    count: number
+    next: string
+    previous: string
+    results: Result[]
 }
+  
+interface Game {
+    id: number
+    slug: string
+    name: string
+    released: string
+    tba: boolean
+    background_image: string
+    rating: number
+    rating_top: number
+    ratings: Ratings
+    ratings_count: number
+    reviews_text_count: string
+    added: number
+    added_by_status: AddedByStatus
+    metacritic: number
+    playtime: number
+    suggestions_count: number
+    updated: string
+    esrb_rating: EsrbRating
+    platforms: Platform[]
+}
+  
+interface Ratings {}
+  
+interface AddedByStatus {}
+  
+interface EsrbRating {
+    id: number
+    slug: string
+    name: string
+}
+  
+interface Platform {
+    platform: PlatformDesc
+    released_at: string
+    requirements: Requirements
+}
+  
+interface PlatformDesc {
+    id: number
+    slug: string
+    name: string
+}
+  
+interface Requirements {
+    minimum: string
+    recommended: string
+}
+  
--- stores/games.ts
+++ stores/games.ts
@@ -1,14 +1,26 @@
+interface State {
+    gamesList: Game[] | undefined,
+    nextPageUrl: string | null 
+}
+
 export const useGamesStore = defineStore('games', {
-    state: (): any => {
+    state: (): State => {
         return {
             gamesList: [],
+            nextPageUrl: null,
         }
     },
 
     actions: {
         async getGamesList() {
-            const resp: any = await useGamesFetch('/games')
-            this.gamesList = resp.data.value.results
+            const { data } = await useGamesFetch<GamesResponse>('/games')
+            this.gamesList = data.value.results
+            this.nextPageUrl = data.value.next
+        },
+        async getNextPage() {
+            const { data } = await useFetch<GamesResponse>(this.nextPageUrl)
+            this.gamesList = [...this.gamesList, ...data.value.results]
+            this.nextPageUrl = data.value.next
         }
     }
 })
