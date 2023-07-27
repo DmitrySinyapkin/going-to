c2b17c2a8b94dc9400c9670c7479e402f63407d4
DmitrySinyapkin <dsinyapkin@mail.ru>
2023-07-27 18:50:02 +0300

    added login page

---
+++ pages/login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="tw-pt-20 tw-mx-auto">
+        <v-sheet width="300" class="mx-auto">
+            <h2 class="tw-mb-2">LogIn</h2>
+            <v-form ref="form" fast-fail @submit.prevent="onSubmit">
+                <v-text-field
+                    v-model="email"
+                    label="E-mail"
+                    :rules="[required, isEmail]"
+                ></v-text-field>
+
+                <v-text-field
+                    v-model="password"
+                    label="Password"
+                    :rules="[required]"
+                ></v-text-field>
+
+                <v-btn type="submit" block class="mt-2">Log In</v-btn>
+            </v-form>
+        </v-sheet>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { VNodeRef } from 'nuxt/dist/app/compat/capi';
+    import { required, isEmail } from '@/utils/validationRules'
+
+    definePageMeta({
+        layout: 'main',
+    })
+
+    const form = ref<VNodeRef | null>(null)
+    const email = ref<string>('')
+    const password = ref<string>('')
+    const supabase = useSupabaseAuthClient()
+    const user = useSupabaseUser()
+    const router = useRouter()
+
+    const onSubmit = async() => {
+        const { valid } = await form.value.validate()
+
+        if (valid) {
+            const { error } = await supabase.auth.signInWithPassword({
+                email: email.value,
+                password: password.value
+            })
+
+            if (error) alert(`Login failed! ${error}`)
+        }
+    }
+    
+    watchEffect(async() => {
+        if (user.value) {
+            await router.push('/')
+        }
+    })
+
+</script>
