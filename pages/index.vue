6340f25d4a29201d993efa81fcc10cdc70dbd1cc
DmitrySinyapkin <dsinyapkin@mail.ru>
2023-07-28 12:43:35 +0300

    improved home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="tw-p-2">
-    <h1>Home</h1>
-    <div>
-      <nuxt-link to="/games">games</nuxt-link>
+  <div class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-pt-10">
+    <h1 class="tw-mb-10">Welcome to GOING-TO App</h1>
+    <div v-if="user" class="tw-mt-6">
+      <SidebarMenu />
+    </div>
+    <div v-else>
+      <v-list>
+        <v-list-item
+          variant="plain"
+          to="/login"
+          title="Log in" 
+        />
+        <v-list-item
+          variant="plain"
+          to="/signup"
+          title="Sign Up" 
+        />
+      </v-list>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import SidebarMenu from '@/components/common/SidebarMenu.vue';
+
   definePageMeta({
     layout: 'main',
   })
+
+  const user = useSupabaseUser()
 </script>
