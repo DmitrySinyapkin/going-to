b040645aa02f8f4f8bbba4725f0a51409b29597b
DmitrySinyapkin <dsinyapkin@mail.ru>
2023-07-31 17:10:28 +0300

    improved default layout for mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,18 @@
   <v-app>
     <v-app-bar>
         <div class="tw-w-full tw-h-full tw-px-5 tw-flex tw-justify-between tw-items-center tw-bg-black tw-text-white">
-          <nuxt-link class="tw-text-white" to="/">GOING-TO</nuxt-link>
+          <nuxt-link v-if="mdAndUp" class="tw-text-white" to="/">GOING-TO</nuxt-link>
+          <v-menu v-else>
+            <template v-slot:activator="{ props }">
+              <v-btn icon="mdi-dots-vertical" v-bind="props"></v-btn>
+            </template>
+
+            <SidebarMenu />
+          </v-menu>
           <UserTopMenu />
         </div>
     </v-app-bar>
-    <v-navigation-drawer class="bg-gray-200 pt-20" permanent disable-resize-watcher>
+    <v-navigation-drawer class="bg-gray-200 pt-20" :permanent="mdAndUp">
         <SidebarMenu />
     </v-navigation-drawer>
     <v-main class="bg-gray pt-40">
@@ -18,4 +25,7 @@
 <script setup>
   import SidebarMenu from '@/components/common/SidebarMenu.vue';
   import UserTopMenu from '@/components/common/UserTopMenu.vue';
+  import { useDisplay } from 'vuetify';
+
+  const { mdAndUp } = useDisplay()
 </script>
